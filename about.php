e7c51e63a1895284205f3eed353e4e63125226e9
NathanielKiremitciyan <105924273@student.swin.edu.au>
2025-05-12 13:03:33 +1000

    FullstopsAboutAllocation

--- about.php
+++ about.php
@@ -137,16 +137,16 @@
               <h1>Who Did What</h1>
               <dl>
                 <dt>Nathaniel</dt>
-                <dd><br>Built the index and plans page as well as preventing the site from becoming too professional
+                <dd><br>Built the index and plans page as well as preventing the site from becoming too professional.
                 <br>Also worked on Tasks 1,2,6 and 8 for Part2<br></dd>
                 <dt><br>Harry</dt>
-                <dd><br>Crafted the job application page and helped ground our vision in reality
+                <dd><br>Crafted the job application page and helped ground our vision in reality.
                 <br>Also worked on Task 5 for Part2<br></dd>
                 <dt<br>>Krish</dt>
-                <dd><br>Assembled job descriptions and sprinkled in the corporate tone
+                <dd><br>Assembled job descriptions and sprinkled in the corporate tone.
                 <br>Also worked on Task 7 for Part2<br></dd>
                 <dt><br>Aiden</dt>
-                <dd><br>Wrote the about page, formatted it with love, and added just the right amount of chaos
+                <dd><br>Wrote the about page, formatted it with love, and added just the right amount of chaos.
                 <br>Also worked on Tasks 3 and 4 for Part2<br></dd>
               </dl>
             </div>
