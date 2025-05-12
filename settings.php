e91c72dffbc4720e901ae6d295efb8cb4660d2ca
NathanielKiremitciyan <105924273@student.swin.edu.au>
2025-05-12 12:49:01 +1000

    addedsettings

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";         // because XAMPP runs the server locally
+$username = "root";          // default username for XAMPP's MySQL
+$password = "";              // default password is empty in XAMPP
+$database = "";              // replace with the actual name of your database
+
+$conn = mysqli_connect($host,$username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
